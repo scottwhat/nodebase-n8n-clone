30e6987de96acef11ec4dc218bbf014b0d1ca7b7
{your_name} <{your_email}>
2025-12-27 19:26:09 +1100

    First commit + setup

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+
+const Page = () => {
+  return (
+    <div className="min-h-screen min-w-screen flex items-center justify-center">
+        <Button variant="outline">Click</Button>
+      </div>
+  )
+}
+
+export default Page
+
+
+
